bcf6e9fa80aab3969cafaa9a0bd8890469ea8fa9
root <root@ip-172-31-92-114.ec2.internal>
2025-02-23 03:03:20 +0000

    a dem is add

---
+++ dem.py
@@ -0,0 +1,2 @@
+Multiplication Charts (PDF): Free Printable Times Tables
+Free printable multiplication charts (times tables) available in PDF format. These colorful charts can be downloaded and printed for use at home or school making multiplication practice more accessible and engaging. For more ideas see printable paper and math drills and math problems generator
